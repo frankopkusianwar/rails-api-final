e019588a47a83e94e1c3593989e47289162f7649
frankopkusianwar <okirorfrank3@gmail.com>
2021-06-14 12:56:53 +0300

    Add product model

---
+++ db/migrate/20210614095614_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :slug
+      t.string :metaTitle
+      t.text :summary
+      t.int :type
+      t.string :sku
+      t.decimal :price
+      t.decimal :discount
+      t.int :quantity
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
