6b1c06776e57d55217b80d4662001ea1861e0879
frankopkusianwar <okirorfrank3@gmail.com>
2021-06-14 14:47:40 +0300

    Complete categories associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-    has_many :productCategories
+    has_many :product_categories
+    has_many: :products, :through => :product_categories
     has_many :children, :class_name => "Category", foreign_key: 'parent_id'
     belongs_to :parent, :class_name => "Category", foreign_key: 'parent_id', :optional => true
 end
