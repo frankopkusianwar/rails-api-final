f9fe7a797b0cd857828600f2d2f8e50606e7f00a
frankopkusianwar <okirorfrank3@gmail.com>
2021-06-14 15:12:23 +0300

    Add product meta associations

---
+++ app/models/product_meta.rb
@@ -0,0 +1,3 @@
+class ProductMeta < ApplicationRecord
+    belongs_to :product
+end
--- db/migrate/20210614095927_create_product_meta.rb
+++ db/migrate/20210614095927_create_product_meta.rb
@@ -1,10 +1,11 @@
 class CreateProductMeta < ActiveRecord::Migration[6.1]
   def change
-    create_table :product_meta do |t|
+    create_table :product_metas do |t|
       t.string :key
       t.text :content
 
       t.timestamps
     end
+    add_reference :product_metas, :product, index: true
   end
 end
