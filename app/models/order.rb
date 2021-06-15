a699b5c48a5a56271cd0d9fc47f306d38f9b5970
frankopkusianwar <okirorfrank3@gmail.com>
2021-06-15 14:18:31 +0300

    Correct typos

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :transactions
     has_many :order_items
-    has_many: :products, :through => :order_items
+    has_many :products, :through: :order_items
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
     has_many :product_metas
     has_many :product_reviews
     has_many :order_items
-    has_many: :orders, :through => :order_items
-    has_many: cart_items
-    has_many: :carts, :through => :cart_items
+    has_many :orders, through: :order_items
+    has_many :cart_items
+    has_many :carts, through: :cart_items
     has_many :product_categories
-    has_many: :categories, :through => :product_categories
+    has_many :categories, through: :product_categories
 end
