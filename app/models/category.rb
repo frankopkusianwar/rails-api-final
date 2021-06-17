71bdbfee2e6de8a8c332fc8da5a62bc600e81e97
frankopkusianwar <okirorfrank3@gmail.com>
2021-06-17 17:29:43 +0300

    Fix seed bug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     has_many :product_categories
     has_many :products, through: :product_categories
-    has_many :children, class_name: "Category", foreign_key: 'parent_id', optianal: true
-    belongs_to :parent, class_name: "Category"
+    has_many :children, class_name: "Category"
+    belongs_to :parent, class_name: "Category", foreign_key: :parent_id, optional: true
 end
